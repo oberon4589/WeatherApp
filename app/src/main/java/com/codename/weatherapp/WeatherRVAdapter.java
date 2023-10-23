3d74dd63bb3ff26273bc7ce370f22e379fe24066
SKINKA <oberon4589@gmail.com>
2023-10-23 00:21:16 -0300

    feat: Atualizar adaptador para RecyclerView com dados reais

--- app/src/main/java/com/codename/weatherapp/WeatherRVAdapter.java
+++ app/src/main/java/com/codename/weatherapp/WeatherRVAdapter.java
@@ -1,16 +1,31 @@
 package com.codename.weatherapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModal> weatherRVModalArrayList) {
+        this.context = context;
+        this.weatherRVModalArrayList = weatherRVModalArrayList;
+    }
+
     @NonNull
     @Override
     public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -20,12 +35,19 @@ public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.View
 
     @Override
     public int getItemCount() {
-        return 0;
+        return weatherRVModalArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView windTV, temperatureTV, timeTV;
+        private ImageView conditionIV;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            windTV = itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV = itemView.findViewById(R.id.idTVTemperature);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            conditionIV = itemView.findViewById(R.id.idIVCondition);
         }
     }
 }
