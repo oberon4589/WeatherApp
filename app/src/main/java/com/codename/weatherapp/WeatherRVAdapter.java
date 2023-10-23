16a946282bace36e87db805e24d6f0710a2146e9
SKINKA <oberon4589@gmail.com>
2023-10-23 00:10:43 -0300

    feat: Adicionar adaptador para RecyclerView

--- app/src/main/java/com/codename/weatherapp/WeatherRVAdapter.java
+++ app/src/main/java/com/codename/weatherapp/WeatherRVAdapter.java
@@ -1,4 +1,31 @@
 package com.codename.weatherapp;
 
-public class WeatherRVAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
