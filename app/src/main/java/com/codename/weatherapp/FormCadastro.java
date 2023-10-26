4d2092ac4bb521c2c71ce906186d30277e6d1540
SKINKA <oberon4589@gmail.com>
2023-10-26 14:17:36 -0300

    feat: recuperando dados do usuário

--- app/src/main/java/com/codename/weatherapp/FormCadastro.java
+++ app/src/main/java/com/codename/weatherapp/FormCadastro.java
@@ -3,12 +3,44 @@ package com.codename.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private EditText edit_nome, edit_email, edit_senha;
+    private Button bt_cadastrar;
+    String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        iniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
+                }
+
+            }
+        });
+    }
+
+    private void iniciarComponentes(){
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_cadastrar = findViewById(R.id.bt_cadastrar);
     }
 }
