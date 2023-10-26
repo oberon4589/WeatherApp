830e1677e3e8091ecfdc0467613d908e1cb7f3ac
SKINKA <oberon4589@gmail.com>
2023-10-26 14:41:40 -0300

    feat: recuperando dados do usuário

--- app/src/main/java/com/codename/weatherapp/FormCadastro.java
+++ app/src/main/java/com/codename/weatherapp/FormCadastro.java
@@ -1,13 +1,20 @@
 package com.codename.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormCadastro extends AppCompatActivity {
 
@@ -29,15 +36,33 @@ public class FormCadastro extends AppCompatActivity {
                 String email = edit_email.getText().toString();
                 String senha = edit_senha.getText().toString();
 
-                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(v, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                    Toast.makeText(FormCadastro.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    CadastrarUsuario();
                 }
-
             }
         });
     }
 
-    private void iniciarComponentes(){
+    private void CadastrarUsuario(){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                     Toast.makeText(FormCadastro.this, "Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show();
+                     finish();
+                }else{
+                     Toast.makeText(FormCadastro.this, "Erro ao cadastrar usuário", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void iniciarComponentes() {
         edit_nome = findViewById(R.id.edit_nome);
         edit_email = findViewById(R.id.edit_email);
         edit_senha = findViewById(R.id.edit_senha);
