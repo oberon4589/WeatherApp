a0125011f7b13f2e11f6acc3bd66185c5623492f
SKINKA <oberon4589@gmail.com>
2023-11-13 21:40:34 -0300

    feat: adicionando extra UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,18 +12,19 @@ android {
         multiDexEnabled = true
     }
 
-    packagingOptions {
-        pickFirst("androidsupportmultidexversion.txt")
-    }
-
     defaultConfig {
         applicationId = "com.codename.weatherapp"
-        minSdk = 19
-        targetSdk = 34
+        minSdk = 24
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    packagingOptions {
+        resources {
+            pickFirsts += setOf("androidsupportmultidexversion.txt")
+        }
     }
 
     buildTypes {
@@ -43,14 +44,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.android.support:multidex:1.0.3")
     implementation ("androidx.multidex:multidex:2.0.1")
+    implementation ("com.google.android.material:material:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
